0d5800ceeb54c1219fa1980928e263e8bdd19d2a
Suraj-Gov <surajgovind332000@gmail.com>
2020-09-04 14:05:20 +0530

    Updates

---
+++ precache-manifest.188642cba393d7cef7ea61ea9198d7a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802b0e62360409182235779ede6d59d2",
+    "url": "/corpo-wars-auction/index.html"
+  },
+  {
+    "revision": "81de683e7d4592ef888c",
+    "url": "/corpo-wars-auction/static/js/2.6532d0a3.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/corpo-wars-auction/static/js/2.6532d0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042be7818618227ef91e",
+    "url": "/corpo-wars-auction/static/js/main.773d3351.chunk.js"
+  },
+  {
+    "revision": "66cb3468c8b1b50ff993",
+    "url": "/corpo-wars-auction/static/js/runtime-main.38e6591e.js"
+  }
+]);
